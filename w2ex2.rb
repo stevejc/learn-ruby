0091a70df7db8fe456d98a11bb675bbb0abacbb3
Steve <stevemargc@gmail.com>
2012-08-31 16:39:49 -0400

    added w2 ex 2 and 3

---
+++ w2ex2.rb
@@ -0,0 +1,20 @@
+puts "Version 1"
+
+def mtdarry
+  10.times do |num|
+    puts num
+  end
+
+end
+
+mtdarry
+
+puts "Version 2"
+
+def mtdarry
+  10.times do |num|
+    puts "hi"
+  end
+end
+
+puts mtdarry
