f03f217adbd23fc36935c30aad4c765974485e6a
Steve <stevemargc@gmail.com>
2012-12-21 12:20:24 -0500

    rewrote code to only use string

--- week2ex1.rb
+++ week2ex1.rb
@@ -1,8 +1,6 @@
 s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n"
 
-lines = s.split("\n")
-
-lines.to_enum.with_index(1) {|line, line_num| puts "Line #{line_num}: #{line}"}
+s.lines.with_index(1) {|line, line_num| puts "Line #{line_num}: #{line}"}
 
 
 
