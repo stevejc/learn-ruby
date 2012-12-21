7e69b6ed064de8f940020f55864801787eea9226
Steve <stevemargc@gmail.com>
2012-12-21 12:17:32 -0500

    rewrote code to only use strings

--- week2c3.rb
+++ week2c3.rb
@@ -3,15 +3,25 @@ def linefy(text)
   number_of_lines = lines.length.to_s
   spacing = number_of_lines.length
   lines.each_with_index.map do |line, line_num|
-    line =  "Line #{line_num.next.to_s.rjust(spacing)}: #{line}"
+    text +=  "Line #{line_num.next.to_s.rjust(spacing)}: #{line}\n"
   end
+  
+  text
+end
+
+def linefy2(text)
+  linedfied_text = ""
+  text.lines.with_index(1) {|line, line_num| linedfied_text += "Line #{line_num.to_s.rjust(text.lines.count.to_s.length)}: #{line}"}
+  linedfied_text
 end
 
 s = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\nDuis imperdiet sem eu quam.\nDonec bibendum tincidunt purus.\nNunc eu tellus sed turpis volutpat pellentesque.\nNunc accumsan varius elit.\nAenean sit amet magna eget odio ornare vulputate.\nUt ullamcorper tellus non magna.\nSed non arcu vel libero posuere ultricies.\nSuspendisse tincidunt ullamcorper tellus.\nIn ornare lacus ut turpis.\n"
 
-s_array = linefy(s)
+#text = linefy(s)
+text2 = linefy2(s)
 
-puts s_array
+puts text2
+
 
 
 
